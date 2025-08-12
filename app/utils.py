5a47b8e8eadbb62cf2c8243372230a0c426138d8
bilmoBaggins <bilalmohammad500123@gmail.com>
2025-08-12 14:36:40 +0100

    removed wiki tool

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,4 @@
-from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
-from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_community.tools import DuckDuckGoSearchRun
 from langchain.tools import Tool
 from datetime import datetime
 
@@ -33,6 +32,3 @@ search_tool = Tool(
     func=search.run,
     description="Search the web for information.",
 )
-
-api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
-wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
