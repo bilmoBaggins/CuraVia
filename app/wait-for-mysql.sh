5fad8a36e817d7740ca1d03a228ec32421562dcb
bilmoBaggins <bilalmohammad500123@gmail.com>
2026-09-04 20:44:03 +0100

    Create CuraVia parent with project submodules

--- app/wait-for-mysql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-}
-
-until mysqladmin ping -h mysql_service -u root -p"$MYSQL_ROOT_PASSWORD" --ssl=OFF --silent; do
-  echo "Waiting for MySQL..."
-  sleep 2
-done
-exec alembic upgrade head
